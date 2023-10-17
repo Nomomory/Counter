c9e4a02c53cf7903c79ac795758280db95920f0a
Andrey Lastushkin <nomomory@yandex.ru>
2023-10-17 18:30:56 +0700

    Создал кнопку и лейбл для подсчета счетчика Добавил метод подсчета

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+private var count = 0
+    @IBOutlet weak var counterButtonBiew: UIButton!
+    @IBOutlet weak var counterLabelView: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        counterLabelView.text = "Значение счетчика: \(count)"
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func counterLabel(_ sender: Any) {
+        count += 1
+        counterLabelView.text = "Значение счетчика: \(count)"
+    }
+    
 }
 
